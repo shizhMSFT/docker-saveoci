8b19f523f5eaf969b98f10c3ce3ae7d4e6f5460e
Shiwei Zhang <shizh@microsoft.com>
2023-05-04 14:18:42 +0800

    metadata command
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- cmd/docker-save-oci/main.go
+++ cmd/docker-save-oci/main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/spf13/cobra"
+)
+
 func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	cmd := &cobra.Command{
+		Use:          "docker-save-oci",
+		Short:        "A docker plugin to save one or more images to a tar archive in the OCI layout",
+		SilenceUsage: true,
+	}
+	cmd.AddCommand(
+		metadataCommand(),
+	)
+	if err := cmd.ExecuteContext(ctx); err != nil {
+		os.Exit(1)
+	}
 }
